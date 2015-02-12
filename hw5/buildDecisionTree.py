a77f1ec54bcee09b3b4a9a425413b8e4d356581c
xiliuhk <xiliuhk@gmail.com>
2015-02-11 21:38:34 -0500

    initial

---
+++ hw5/buildDecisionTree.py
@@ -0,0 +1,17 @@
+import math
+import sys
+import os
+
+def infoGain(instance):
+	gain = 0
+	return gain 
+
+def createInstance:
+	instance = []
+	return instance
+
+def buildDecisionTree():
+	reuturn DT
+
+def _main():
+	
